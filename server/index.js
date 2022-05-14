59342703fc1a226f171ba30477141ebbd754cd7d
Marian <marianlopig@gmail.com>
2022-05-14 12:46:16 +0200

    adding the server connection with an empty array to a localhost

--- server/index.js
+++ server/index.js
@@ -4,11 +4,9 @@ const morgan = require("morgan");
 const initializeServer = require("./initializeServer");
 const robotsRouter = require("./routers/robotsRouter");
 
-console.log("initializing");
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/robots", robotsRouter);
 
 initializeServer(app, process.env.PORT || 4000);
-console.log("initialized completed");
