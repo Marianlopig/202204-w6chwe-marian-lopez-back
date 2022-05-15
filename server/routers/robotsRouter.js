918f7de44c060c5af89a5984fbcf4495f82858d2
Marian <marianlopig@gmail.com>
2022-05-15 11:09:55 +0200

    delete method added in robotsRouter

--- server/routers/robotsRouter.js
+++ server/routers/robotsRouter.js
@@ -3,9 +3,18 @@ const express = require("express");
 const robot = require("../../db");
 const router = express.Router();
 
+router.get("/", getRobots);
+router.delete("/delete/:idRobot", deleteRobot);
+
 const getRobots = async (req, res) => {
   const robots = await robot.find();
   res.status(200).json(robots);
 };
-router.get("/", getRobots);
-module.exports = { router, getRobots };
+
+const deleteRobot = async (req, res) => {
+  const { robotId } = req.params;
+  await robot.deleteOne({ _id: robotId });
+  res.status(200).json({ _id: robotId });
+};
+
+module.exports = { router, getRobots, deleteRobot };
