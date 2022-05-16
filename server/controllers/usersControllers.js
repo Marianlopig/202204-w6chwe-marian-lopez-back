085c7c667b75c9f818e328674a39eb6499afe455
Marian <marianlopig@gmail.com>
2022-05-16 23:43:26 +0200

    adding auth middleware to robots router

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -1,13 +1,18 @@
 const jwt = require("jsonwebtoken");
+const myUserId = "1";
 const myUserName = "Marian";
 const myPassword = "password";
 
 const userLogin = async (req, res) => {
   const { username, password } = req.body;
   if (username === myUserName && password === myPassword) {
-    const token = jwt.sign({ username }, process.env.TOKEN_SECRET, {
-      expiresIn: "1800s",
-    });
+    const token = jwt.sign(
+      { id: myUserId, username },
+      process.env.TOKEN_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
     res.status(200).json({ token });
   } else {
     res.status(401).json({ msg: "bad request" });
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,10 +1,11 @@
 const jwt = require("jsonwebtoken");
+const debug = require("debug")("robots:users");
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
   const token = authorization?.replace("Bearer ", "");
 
-  if (token === null) return res.sendStatus(401);
+  if (token === null || token === "") return res.sendStatus(401);
 
   jwt.verify(token, process.env.TOKEN_SECRET, (error, user) => {
     debug(error);
@@ -15,3 +16,4 @@ const auth = (req, res, next) => {
     next();
   });
 };
+module.exports = { auth };
