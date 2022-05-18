23957306a7000e53237ff488fbfb4b620b516a4d
Marian <marianlopig@gmail.com>
2022-05-18 16:29:13 +0200

    register controller created

--- db/models/User.js
+++ db/models/User.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique },
   password: { type: String, required: true },
 });
 
